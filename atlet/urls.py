2c76e3241cc1877d6be4b4bcffe242313d3dfb5a
amiransht <amira19nisrina@gmail.com>
2023-05-04 15:59:01 +0700

    set base color

--- atlet/urls.py
+++ atlet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from atlet.views import daftar_sponsor
+from atlet.views import daftar_sponsor, form_tes_kualifikasi
 
 app_name = 'atlet'
 
 urlpatterns = [
     path('daftar-sponsor/', daftar_sponsor, name='daftar_sponsor' ),
+    path('tes-kualifikasi/', form_tes_kualifikasi, name='form_tes_kualifikasi' ),
 
 ]
--- atlet/views.py
+++ atlet/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def daftar_sponsor(request):
     return render(request, "daftar_sponsor.html")
+
+def form_tes_kualifikasi(request):
+    return render(request, "form_tes_kualifikasi.html")
+
