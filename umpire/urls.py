7f2e8684eb5a3c352225434628c4720405f109e9
amiransht <amira19nisrina@gmail.com>
2023-05-05 17:18:14 +0700

    petandingan umpire

--- umpire/urls.py
+++ umpire/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from umpire.views import list_event, dashboard_umpire, hasil_pertandingan, detail_hasil_pertandingan
+from umpire.views import list_event, dashboard_umpire, hasil_pertandingan, detail_hasil_pertandingan, pertandingan
 
 app_name = 'umpire'
 
 urlpatterns = [
     path('list-event/', list_event, name='list_event'),
+    path('list-event/pertandingan/', pertandingan, name='pertandingan'),
     path('hasil-pertandingan/', hasil_pertandingan, name='hasil_pertandingan'),
     path('hasil-pertandingan/detail/', detail_hasil_pertandingan, name='detail_hasil_pertandingan'),
     path('', dashboard_umpire, name='dashboard_umpire' ),
--- umpire/views.py
+++ umpire/views.py
@@ -18,3 +18,5 @@ def hasil_pertandingan(request):
     return render(request, "hasil_pertandingan.html")
 def detail_hasil_pertandingan(request):
     return render(request, "detail_hasil_pertandingan.html")
+def pertandingan(request):
+    return render(request, "pertandingan.html")
